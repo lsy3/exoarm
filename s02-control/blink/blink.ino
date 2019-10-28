52e6e6eba30bb5cf4e421dd3cd91897426ae4912
Luke Sy <sylukewicent@gmail.com>
2019-10-28 11:30:32 +1100

    pdf link fix

--- s02-control/blink/blink.ino
+++ s02-control/blink/blink.ino
@@ -20,13 +20,13 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin PB1 as an output.
-  pinMode(PB12, OUTPUT);
+  pinMode(PC13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(PB12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
-  digitalWrite(PB12, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
